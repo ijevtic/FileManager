a7b4a05c1c735d15dd0305e8b54f2dab4508b9c0
Ivan Jevtic <ivanjevtic501@gmail.com>
2022-10-23 21:00:51 +0200

    restore

--- Specification/documentation/member-search-index.js
+++
@@ -1,1 +0,0 @@
-memberSearchIndex = [{"p":"org.raf.specification","c":"FileManager","l":"downloadFiles(List<String>, String)","u":"downloadFiles(java.util.List,java.lang.String)"},{"p":"org.raf.specification","c":"FileManager","l":"FileManager()","u":"%3Cinit%3E()"}];updateSearchResults();
