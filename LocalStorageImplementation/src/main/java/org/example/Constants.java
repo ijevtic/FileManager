7311ba846402173333afa3cfb4a7cf030fa42f2d
Ivan Jevtic <ivanjevtic501@gmail.com>
2022-10-30 15:00:36 +0100

    Add createDirectory implementation for LocalStorage

--- LocalStorageImplementation/src/main/java/org/example/Constants.java
+++ LocalStorageImplementation/src/main/java/org/example/Constants.java
@@ -1,2 +1,11 @@
-package org.example;public class Constants {
+package org.example;
+
+import org.raf.specification.Configuration;
+
+import java.util.List;
+
+public class Constants {
+    public static String STORAGE_PATH  = "/home/ijevtic/Desktop/";
+    public static String STORAGE_NAME = "PredefinedName";
+    public static Configuration CONFIGURATION = new Configuration(10000, List.of("exe"), 10);
 }
--- LocalStorageImplementation/src/main/java/org/example/Implementation.java
+++ LocalStorageImplementation/src/main/java/org/example/Implementation.java
@@ -1,17 +1,81 @@
 package org.example;
 
+import org.raf.specification.Configuration;
 import org.raf.specification.FileManager;
+import org.raf.specification.SpecFile;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
-public class Implementation extends FileManager {
+import static org.example.Constants.*;
+
+public class Implementation extends FileManager{
+
     @Override
-    public int downloadFiles(List<String> list, String s) {
-        return 0;
+    public void createStorage() {
+        createStorage(CONFIGURATION, STORAGE_PATH, STORAGE_NAME);
+    }
+    @Override
+    public void createStorage(String path, String name) {
+        createStorage(CONFIGURATION, path, name);
+    }
+    @Override
+    public void createStorage(String name) {
+        createStorage(CONFIGURATION, STORAGE_PATH, name);
+    }
+    @Override
+    public void createStorage(Configuration configuration) {
+        createStorage(configuration, STORAGE_PATH, STORAGE_NAME);
+    }
+    @Override
+    public void createStorage(Configuration configuration, String name) {
+        createStorage(configuration, STORAGE_PATH, name);
+    }
+    @Override
+    public void createStorage(Configuration configuration, String path, String name) {
+        try {
+            Path dirPath = Paths.get(path + name);
+
+            Files.createDirectories(dirPath);
+
+            System.out.println("Storage is created!");
+
+        } catch (IOException e) {
+
+            System.err.println("Failed to create storage!" + e.getMessage());
+        }
     }
 
     @Override
-    public void lol() {
+    public void createDirectory(String s, String s1) {
+
+    }
+
+    @Override
+    public void addFiles(String s, List<SpecFile> list) {
+
+    }
+
+    @Override
+    public void removeFile(String s) {
+
+    }
+
+    @Override
+    public void moveFiles(List<SpecFile> list, String s) {
+
+    }
+
+    @Override
+    public void download(String s, String s1) {
+
+    }
+
+    @Override
+    public void rename() {
 
     }
 }
