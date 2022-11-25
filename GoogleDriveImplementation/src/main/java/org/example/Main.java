244cfa934e1e065706b2825549383f217a4dbfa6
NtNklc <nikolic.nt@gmail.com>
2022-11-25 19:13:07 +0100

    Fix minor bugs, test deive implementation

--- GoogleDriveImplementation/src/main/java/org/example/Main.java
+++ GoogleDriveImplementation/src/main/java/org/example/Main.java
@@ -1,17 +1,15 @@
 package org.example;
 
 import org.example.example.Implementation;
+import org.raf.specification.ESortingOrder;
+import org.raf.specification.SortingCriteria;
 import org.raf.specification.SpecFile;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        Implementation im = new Implementation();
-//        im.createStorage("/lolcina", "nesto");
-//        SpecFile s = new SpecFile("/home/ijevtic/Desktop/proba.pdf", "proba.pdf");
-//        SpecFile dest = new SpecFile("/lolcina/lol1", "lol1");
-//        im.addFiles(List.of(s), dest.getPath());
     }
 }
--- Specification/src/main/java/org/raf/utils/Constants.java
+++ Specification/src/main/java/org/raf/utils/Constants.java
@@ -5,7 +5,7 @@ import org.raf.specification.Configuration;
 import java.util.List;
 
 public class Constants {
-    public static String STORAGE_PATH  = "/home/ijevtic/Desktop/";
+    public static String STORAGE_PATH  = "/home/nata/Desktop/test1/";
     public static String STORAGE_NAME = "PredefinedName";
     public static Configuration CONFIGURATION = new Configuration(STORAGE_PATH+STORAGE_NAME, 10000, List.of("exe"), 10);
 }
